a9a5670114ed8829f3bf1cbfc42131fb7637479b
Advit pikameow420 <lci2021045@iiitl.ac.in>
2024-08-25 22:59:09 +0530

    Added whitelisted domains

--- src/app/web3onboard.ts
+++ src/app/web3onboard.ts
@@ -1,85 +1,83 @@
-"use client"
+"use client";
 
-import safeModule from '@web3-onboard/gnosis'
-import injectedModule from '@web3-onboard/injected-wallets'
-import { init } from '@web3-onboard/react'
+import safeModule from "@web3-onboard/gnosis";
+import injectedModule from "@web3-onboard/injected-wallets";
+import { init } from "@web3-onboard/react";
 
-const INFURA_KEY = process.env.NEXT_PUBLIC_INFURA_ID
+const INFURA_KEY = process.env.NEXT_PUBLIC_INFURA_ID;
 
-const injected = injectedModule()
+const injected = injectedModule();
 
-const safe = safeModule()
-
+const safe = safeModule({
+  whitelistedDomains: [/app\.safe\.global$/, /coinshift\.global$/],
+});
 
 export default init({
   // An array of wallet modules that you would like to be presented to the user to select from when connecting a wallet.
-  wallets: [
-    injected,
-    safe,
-  ],
+  wallets: [injected, safe],
   // An array of Chains that your app supports
   chains: [
     {
       // hex encoded string, eg '0x1' for Ethereum Mainnet
-      id: '0x1',
+      id: "0x1",
       // string indicating chain namespace. Defaults to 'evm' but will allow other chain namespaces in the future
-      namespace: 'evm',
+      namespace: "evm",
       // the native token symbol, eg ETH, BNB, MATIC
-      token: 'ETH',
+      token: "ETH",
       // used for display, eg Ethereum Mainnet
-      label: 'Ethereum Mainnet',
+      label: "Ethereum Mainnet",
       // used for network requests
-      rpcUrl: `https://mainnet.infura.io/v3/${INFURA_KEY}`
+      rpcUrl: `https://mainnet.infura.io/v3/${INFURA_KEY}`,
     },
     {
       id: 42161,
-      token: 'ARB-ETH',
-      label: 'Arbitrum One',
-      rpcUrl: 'https://rpc.ankr.com/arbitrum'
+      token: "ARB-ETH",
+      label: "Arbitrum One",
+      rpcUrl: "https://rpc.ankr.com/arbitrum",
     },
     {
-      id: '0xa4ba',
-      token: 'ARB',
-      label: 'Arbitrum Nova',
-      rpcUrl: 'https://nova.arbitrum.io/rpc'
+      id: "0xa4ba",
+      token: "ARB",
+      label: "Arbitrum Nova",
+      rpcUrl: "https://nova.arbitrum.io/rpc",
     },
     {
-      id: '0x89',
-      token: 'MATIC',
-      label: 'Matic Mainnet',
-      rpcUrl: 'https://matic-mainnet.chainstacklabs.com'
-    }
+      id: "0x89",
+      token: "MATIC",
+      label: "Matic Mainnet",
+      rpcUrl: "https://matic-mainnet.chainstacklabs.com",
+    },
   ],
   appMetadata: {
     // The name of your dApp
-    name: 'Blocknative',
+    name: "Blocknative",
     // SVG icon string, with height or width (whichever is larger) set to 100% or a valid image URL
-    icon: '<svg></svg>',
+    icon: "<svg></svg>",
     // Optional wide format logo (ie icon and text) to be displayed in the sidebar of connect modal. Defaults to icon if not provided
-    logo: '<svg></svg>',
+    logo: "<svg></svg>",
     // The description of your app
-    description: 'Demo app for Onboard V2',
+    description: "Demo app for Onboard V2",
     // The url to a getting started guide for app
-    gettingStartedGuide: 'http://mydapp.io/getting-started',
+    gettingStartedGuide: "http://mydapp.io/getting-started",
     // url that points to more information about app
-    explore: 'http://mydapp.io/about',
+    explore: "http://mydapp.io/about",
     // if your app only supports injected wallets and when no injected wallets detected, recommend the user to install some
     recommendedInjectedWallets: [
       {
         // display name
-        name: 'MetaMask',
+        name: "MetaMask",
         // link to download wallet
-        url: 'https://metamask.io'
+        url: "https://metamask.io",
       },
-      { name: 'Coinbase', url: 'https://wallet.coinbase.com/' }
+      { name: "Coinbase", url: "https://wallet.coinbase.com/" },
     ],
     // Optional - but allows for dapps to require users to agree to TOS and privacy policy before connecting a wallet
     agreement: {
-      version: '1.0.0',
-      termsUrl: 'https://www.blocknative.com/terms-conditions',
-      privacyUrl: 'https://www.blocknative.com/privacy-policy'
-    }
-  }
+      version: "1.0.0",
+      termsUrl: "https://www.blocknative.com/terms-conditions",
+      privacyUrl: "https://www.blocknative.com/privacy-policy",
+    },
+  },
   // example customising copy
   // i18n: {
   //   en: {
@@ -90,4 +88,4 @@ export default init({
   //     }
   //   }
   // }
-})
+});
